f8e31b3a0dc999db3b975a045de9e125ee5f413f
SmartConTractor <login.blockchain888@gmail.com>
2025-03-09 10:24:02 +0100

    create columns, image and text

--- cli.py
+++ cli.py
@@ -1,1 +1,35 @@
+import streamlit as st
+
+st.set_page_config(layout="wide") # expand tu width of the layout
+
+col1, col2 = st.columns(2) # return 2 column objects
+
+with col1:
+    st.image("Portfolio_Icons\\insta_profile_pic2.png", width=400)
+
+with col2:
+    st.title("Gundars Plume")
+
+    content = """
+    A monkey once swung through the trees with delight,
+    Then spotted a panda—fluffy and white.
+    His heart did a flip, his tail curled tight,
+    “I think I’m in love! Oh, what a sight!”\n
+
+    He tried to impress, but tripped on a vine,
+    Panda just laughed and said, “You’ll be fine.”
+    She shared her bamboo, he offered a snack,
+    Bananas for her? She gave them right back!\n
+
+    Though one loved to climb and the other to nap,
+    They fit like two pieces of one perfect map.
+    Love doesn’t care if you're furry or small—
+    Monkey and panda? The best match of all!\n
+    \n
+    -ChatGPT
+    """
+    st.write(content)
+
+
+
 print("PORTFOLIO")
