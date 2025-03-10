22231ea0230b1e90d2bc502b8e86f29b3e200d4c
SmartConTractor <login.blockchain888@gmail.com>
2025-03-10 16:55:25 +0100

    Add email sending through forms.

--- pages/Contact_Me.py
+++ pages/Contact_Me.py
@@ -1,10 +1,18 @@
 import streamlit as st
+from send_email import send_email
 
 st.header("Contact Me")
 
-with st.form(key="e-mail"):
+with st.form(key="email_forms"):
     user_email = st.text_input("Enter your email")
     message = st.text_area("Your message")
+
+    message = f"""\
+Subject: Portfolio Contact Me {user_email}
+{message} \n{user_email}
+"""
+    
     button = st.form_submit_button("Submit")
     if button:
-        print("YES")
+        send_email(message)
+        st.info("E-mail was sent!")
--- send_email.py
+++ send_email.py
@@ -1,25 +1,17 @@
 import smtplib
 import ssl
 
-host = "smtp.gmail.com"
-port = 465
+def send_email(message): # define all email operations as one function
 
-username = "zigmarszigmars19@gmail.com"
-password = "rygl culp blcc smky"
+    host = "smtp.gmail.com"
+    port = 465
 
-receiver = "zigmarszigmars19@gmail.com"
+    username = "zigmarszigmars19@gmail.com"
+    password = "rygl culp blcc smky"
 
+    receiver = "zigmarszigmars19@gmail.com"
+    context = ssl.create_default_context()
 
-context = ssl.create_default_context()
-message = f"""\
-From: {username}
-To: {receiver}
-Subject: Portfolio Contact Me
-
-Hi, how high are you?
-"""
-
-
-with smtplib.SMTP_SSL(host, port, context=context) as server:
-    server.login(username, password) # login as the user of the account
-    server.sendmail(username, receiver, message)
+    with smtplib.SMTP_SSL(host, port, context=context) as server:
+        server.login(username, password) # login as the user of the account
+        server.sendmail(username, receiver, message)
